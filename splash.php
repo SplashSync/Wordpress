25614791117879b76e080e563589e6f4bdb2c97b
BadPixxel <eshop.bpaquier@gmail.com>
2018-09-16 03:07:34 +0200

    Takle a revision

--- splash.php
+++ splash.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Splash Connector
- * Version: 1.4.1
+ * Version: 1.4.2
  * Plugin URI: https://github.com/SplashSync/Wordpress
  * Description: Splash Sync Wordpress plugin.
  * Author: Splash Sync
@@ -21,7 +21,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define("SPLASH_SYNC_VERSION", "1.4.1");
+define("SPLASH_SYNC_VERSION", "1.4.2");
 
 // Load plugin class files
 require_once('includes/class-splash-wordpress-plugin.php');
