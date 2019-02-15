af060f3ad9ed93a31ae8a1192af0ac37deec6349
BadPixxel <eshop.bpaquier@gmail.com>
2019-02-15 01:51:19 +0100

    PhpStan Level 7!

--- includes/class-splash-wordpress-plugin.php
+++ includes/class-splash-wordpress-plugin.php
@@ -238,7 +238,7 @@ class Splash_Wordpress_Plugin
      * @param mixed $file
      * @param mixed $version
      *
-     * @return null|self
+     * @return self
      */
     public static function instance($file = '', $version = SPLASH_SYNC_VERSION)
     {
