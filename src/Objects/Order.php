826572b8757fd473185457643647c219590badd2
BadPixxel <eshop.bpaquier@gmail.com>
2021-03-08 18:33:03 +0100

    UPDATE HEADER 2021

--- src/Objects/Order.php
+++ src/Objects/Order.php
@@ -3,7 +3,7 @@
 /*
  *  This file is part of SplashSync Project.
  *
- *  Copyright (C) 2015-2020 Splash Sync  <www.splashsync.com>
+ *  Copyright (C) 2015-2021 Splash Sync  <www.splashsync.com>
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +17,6 @@ namespace   Splash\Local\Objects;
 
 use Splash\Core\SplashCore      as Splash;
 use Splash\Local\Core\PrivacyManager;
-use Splash\Local\Objects\Core\WooCommerceObjectTrait;
 use Splash\Models\AbstractObject;
 use Splash\Models\Objects\GenericFieldsTrait;
 use Splash\Models\Objects\ImagesTrait;
