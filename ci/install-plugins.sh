7e35bab307e5b6e3a803aef863c312a7d2a51c58
BadPixxel <eshop.bpaquier@gmail.com>
2021-03-08 19:47:23 +0100

    WIP Gitlab CI

--- ci/install-plugins.sh
+++ ci/install-plugins.sh
@@ -27,7 +27,7 @@ echo "* Install Wordpress Additional Plugins..."
 wp plugin install wp-multilang --allow-root
 
 echo "* Install Splash Plugin for Wordpress..."
-cp -Rf "/builds/SplashSync/Wordpress/" "$PLUGIN_DIR"
+mv "/builds/SplashSync/Wordpress/" "$PLUGIN_DIR"
 cd "$PLUGIN_DIR"  || exit
 curl -s https://raw.githubusercontent.com/BadPixxel/Php-Sdk/main/ci/composer.sh | bash
 
